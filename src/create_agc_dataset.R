4a9b8bd21802311fc21244cfae55f28a189a8393
Federico Angelo Mor <federangelomor@gmail.com>
2023-11-18 10:55:43 +0100

    data folder reorder

---
+++ src/create_agc_dataset.R
@@ -0,0 +1,2 @@
+AGC_Dataset = read.csv("../data/AGC_Dataset_v_3_0_0.csv")
+save(AGC_Dataset,file="../data/data_agc_lomb.Rdata")
--- src/include.R
+++ src/include.R
@@ -8,14 +8,14 @@
 # loading procedure, with feedback
 ########################
 library(crayon)
-load("../data/v300/df_agri.Rdata")
+load("../data/df_agri.Rdata")
 cat(crayon::cyan("❤ Loaded agrimonia dataset. Available as df_agri.\n"))
 df_agri$Time <- as.Date(df_agri$Time, "%Y-%m-%d")
 cat(crayon::green("❤ Converted df_agri$Time to Date variable type, year-month-day.\n"))
 DATE_FORMAT = "%Y-%m-%d"
 cat(crayon::green("❤ Created DATE_FORMAT variable for date comparisons.\n"))
 # cat(crayon::green('❤ Usage: ...Time >= as.Date(paste0(year,"-01-01"),DATE_FORMAT).\n'))
-load("../data/v300/df_stat.Rdata")
+load("../data/df_stat.Rdata")
 cat(crayon::cyan("❤ Loaded stations split dataset. Available as df_stat.\n"))
 
 
